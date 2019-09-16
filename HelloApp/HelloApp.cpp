893162c81d53f91f811097f3bd26977f11e1f68a
AndyLishengrui <AndyLishengruiXMU@users.noreply.github.com>
2019-09-16 09:50:20 +0800

    添加项目文件。

---
+++ HelloApp/HelloApp.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello!\n";
+	cout << "world!";
+	return 0;
+
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
